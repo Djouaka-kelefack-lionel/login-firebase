05b121099b5887b6ce91649cba6420a0b2abdf90
Djouaka kelefack lionel <djouakalionel4@gmail.com>
2022-09-17 20:31:46 +0200

    login firebase

---
+++ app/src/main/java/com/injilionel/blocnote/LoginActivity.kt
@@ -0,0 +1,42 @@
+package com.injilionel.blocnote
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.injilionel.blocnote.databinding.ActivityLogin2Binding
+
+class LoginActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityLogin2Binding
+private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener{
+            val intent = Intent(this, LogoutActivity::class.java)
+            startActivity(intent)
+
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if(email.isNotEmpty() && pass.isNotEmpty()){
+                    firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent(this , MainActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Emty Fields Are not Allowed !!" , Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
